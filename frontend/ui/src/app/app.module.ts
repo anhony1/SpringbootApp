26686cfb57d80e67f1ea7f36d3977581b7f8f1b6
anthonyreyes <areyes5141@gmail.com>
2021-11-02 10:56:02 -0500

    3 - adding frontend / ui

---
+++ frontend/ui/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestingComponentComponent } from './testing-component/testing-component.component';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule} from "@angular/material/icon";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestingComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatOptionModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/ui/src/app/testing-component/testing-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-component',
+  templateUrl: './testing-component.component.html',
+  styleUrls: ['./testing-component.component.scss']
+})
+export class TestingComponentComponent implements OnInit {
+
+  value = "Clear me";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
